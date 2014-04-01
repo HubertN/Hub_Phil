5ee855ba85fae83a5743c4831db43815dccc20fc
hubertanguyen@gmail.com <hubertanguyen@gmail.com>
2014-04-01 15:34:42 -0500

    my partner made changes to match.rb, documenting because I don't follow his logic, will go back and look

--- lib/match.rb
+++ lib/match.rb
@@ -1,11 +1,19 @@
 class Match
-  attr_accessor :name, :m_id, :wm_id
+  attr_accessor :name, :m_id, :wm_id, :rwon
   @@id = 0
-  def initialize(name)
+  def initialize(user1,user2)
+    @user1 = user1.id
+    @user2 = user2.id
     @@id += 1
-    @name = name
     @m_id = @@id
     @wm_id = nil
+    @rwon = {}
+  end
+
+  def startmatch(user1,user2)
+    # checks if a user has won 3 rounds
+    # if yes, then update @wm_id to equal userid who has won 3 rounds
+    # a match should have two users, but have many games
   end
 
 
--- spec/match_spec.rb
+++ spec/match_spec.rb
@@ -3,6 +3,7 @@ require "spec_helper"
 describe Match do
 
   it "should initialize with a match name" do
+
     match1 = Match.new("firstmatch")
     match1.name = "firstmatch"
   end
@@ -15,11 +16,21 @@ describe Match do
   it "should start a match, with two users" do
     match1 = Match.new("firstmatch")
 
+    user1 = Users.new("bob")
+    user2 = Users.new("john")
+
+    expect(match1.startmatch(user1,user2)).to eq nil
   end
 
   it "a match should start with nil for match winner" do
     match1 = Match.new("firstmatch")
     expect(match1.wm_id).to eq nil
+  end
+
+  it "start match should return winner user id, if user has won 3 rounds" do
+
+
+
   end
 
 
